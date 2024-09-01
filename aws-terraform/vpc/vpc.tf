30471bcb72e96220addac9cb9de8874043b0f3fc
appavlenko <112694619+appavlenko@users.noreply.github.com>
2024-09-01 21:01:50 +0300

    Update vpc.tf

--- aws-terraform/vpc/vpc.tf
+++ aws-terraform/vpc/vpc.tf
@@ -1,3 +1,27 @@
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
+
+  tags = {
+    Name = "network"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_subnet_cidr
+  availability_zone = "eu-central-1a"
+
+  tags = {
+    Name = "Public-Subnet"
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_cidr
+  availability_zone = "eu-central-1b"
+
+  tags = {
+    Name = "Private-Subnet"
+  }
 }
