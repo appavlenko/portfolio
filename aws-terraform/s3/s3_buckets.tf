40799fd45ece242cb9d2d9d8588545dd200b2796
appavlenko <112694619+appavlenko@users.noreply.github.com>
2024-09-01 21:02:42 +0300

    Update s3_buckets.tf

--- aws-terraform/s3/s3_buckets.tf
+++ aws-terraform/s3/s3_buckets.tf
@@ -1,4 +1,26 @@
 resource "aws_s3_bucket" "app_bucket" {
   bucket = "my-app-bucket"
   acl    = "private"
+
+  logging {
+    target_bucket = "log-bucket"
+    target_prefix = "log/"
+  }
+
+  versioning {
+    enabled = true
+  }
+
+  tags = {
+    Name = "App-Bucket"
+  }
+}
+
+resource "aws_s3_bucket" "log_bucket" {
+  bucket = "my-log-bucket"
+  acl    = "log-delivery-write"
+
+  tags = {
+    Name = "Log-Bucket"
+  }
 }
